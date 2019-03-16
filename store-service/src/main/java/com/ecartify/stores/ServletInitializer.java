3d30d089ad7759952978d95ac77f31bb4f9ac72a
Shariq Islam <shislam@expediagroup.com>
2019-03-16 12:33:05 +0530

    renaming account service

--- store-service/src/main/java/com/ecartify/stores/ServletInitializer.java
+++ store-service/src/main/java/com/ecartify/stores/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.ecartify.accounts;
+package com.ecartify.stores;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(AccountServiceApplication.class);
+		return application.sources(StoreServiceApplication.class);
 	}
 
 }
