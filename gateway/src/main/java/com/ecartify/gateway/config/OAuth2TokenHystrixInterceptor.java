ef81c4a2b734e2e71bcc7567200408c4acc47a20
Shariq Islam <shariqislam786@gmail.com>
2019-03-17 21:02:02 +0530

    maven build fix without tests

--- gateway/src/main/java/com/ecartify/gateway/config/OAuth2TokenHystrixInterceptor.java
+++ gateway/src/main/java/com/ecartify/gateway/config/OAuth2TokenHystrixInterceptor.java
@@ -9,16 +9,19 @@ import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Component
 public class OAuth2TokenHystrixInterceptor implements ClientHttpRequestInterceptor
 {
 
     @Override
-    @HystrixCommand(groupKey = "OAUTH2",commandKey = "TOKEN_REQUEST")
+    @HystrixCommand(groupKey = "OAUTH2", commandKey = "TOKEN_REQUEST", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.strategy", value = "SEMAPHORE")
+    })
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException
     {
-        return execution.execute(request,body);
+        return execution.execute(request, body);
     }
 
 
